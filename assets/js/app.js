9131d287a25e40c38edf94dc82a6c5dc6a0e9efc
SageKyle <ominyipaulo@gmail.com>
2023-04-02 14:47:42 +0100

    add a mobile layout for landing page

---
+++ assets/js/app.js
@@ -0,0 +1,6 @@
+const navBar = document.querySelector('.main-nav');
+const toggle = document.querySelector('.nav-toggle');
+
+toggle.addEventListener('click', (e) => {
+	navBar.classList.toggle('hide-nav');
+});
