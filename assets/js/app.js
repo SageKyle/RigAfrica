0e308bd864ac1c8ea58d6798d9c2c6b907381367
SageKyle <ominyipaulo@gmail.com>
2023-04-04 21:04:49 +0100

    complete animation for contact page

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,7 +1,32 @@
+//================ SCROLLREVEAL ================
+import ScrollReveal from 'scrollreveal';
+ScrollReveal.debug = true;
+
 // ========== NAVIGATION TOGGLE =============
 const navBar = document.querySelector('.main-nav');
 const toggle = document.querySelector('.nav-toggle');
 
 toggle.addEventListener('click', (e) => {
 	navBar.classList.toggle('hide-nav');
+});
+
+// ============= CONTACT PAGE ANIMATION =============
+ScrollReveal().reveal('.center-img', {
+	duration: 800,
+	reset: true,
+	delay: 500,
+	cleanup: true,
+});
+
+ScrollReveal().reveal('.top-img', {
+	duration: 800,
+	reset: true,
+	delay: 800,
+	cleanup: true,
+});
+
+ScrollReveal().reveal('.bottom-img', {
+	duration: 800,
+	reset: true,
+	delay: 1000,
 });
