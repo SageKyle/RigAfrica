00f5422a1de7179ccfb27f9ff0d98dfec603961f
SageKyle <ominyipaulo@gmail.com>
2023-04-25 21:05:08 +0100

    update media queries for homepage

--- assets/js/observer.js
+++ assets/js/observer.js
@@ -1,5 +1,6 @@
 const navContainer = document.querySelector('.navbar');
 const header = document.querySelector('.header__wrapper');
+const newsline = document.querySelector('.newsline');
 
 // ============= INTERSECTION OBSERVER ================
 const options = {
@@ -11,8 +12,12 @@ let observer = new IntersectionObserver((entries) => {
 	entries.forEach((entry) => {
 		if (entry.isIntersecting) {
 			navContainer.classList.remove('header-bg');
+			newsline.classList.remove('hidden');
+			navContainer.style.top = 'inherit';
 		} else {
 			navContainer.classList.add('header-bg');
+			newsline.classList.add('hidden');
+			navContainer.style.top = 0;
 		}
 	});
 }, options);
