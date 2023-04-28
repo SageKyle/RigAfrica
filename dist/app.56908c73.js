f9621f02ffa108c1ccb250a7d5203c94b659c479
SageKyle <ominyipaulo@gmail.com>
2023-04-28 14:26:04 +0100

    update layout of header background for all pages

--- dist/app.56908c73.js
+++ dist/app.56908c73.js
@@ -1965,7 +1965,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "53050" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "55364" + '/');
   ws.onmessage = function (event) {
     checkedAssets = {};
     assetsToAccept = [];
